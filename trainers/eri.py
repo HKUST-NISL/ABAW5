68db94d45b9250033d3ce8ff9fd8c5ca86cd8f95
leon-liangwu <liangwu.lwu@outlook.com>
2023-02-22 11:28:33 +0800

    loss

--- trainers/eri.py
+++ trainers/eri.py
@@ -37,7 +37,7 @@ class ERI(LightningModule):
         # TODO: process the input batch
         # size: batch[0]: {'images': bs, imgRandomLen, 299, 299, 3; 'age': bs; 'country': bs},
         #         batch[1]: np.array: bs, 7;
-        preds = torch.sigmoid(self.head(self.model(batch)))
+        preds = self(imgs)
         loss = F.mse_loss(preds, labels)
         return loss
 
