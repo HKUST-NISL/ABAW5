41bfd05762d493b34964f957dbcba76939548161
yini <fangyini66@outlook.com>
2023-03-27 16:53:30 +0800

    best model

--- test_feat.sh
+++ test_feat.sh
@@ -2,11 +2,11 @@
 # --optimizer adam --lr_scheduler exponential --lr 2e-5 --lr_decay_rate 0.8 \
 # --num_epochs 10 --num_workers 8 --data_dir ~/Data/abaw5 --features $1 --loss l2
 
-python main.py --snippet_size 0 --batch_size 48 --sample_times 1 \
+python main.py --snippet_size 0 --batch_size 10 --sample_times 1 \
 --optimizer adamw --lr_scheduler step --lr 1e-4 \
 --num_epochs 100 --num_workers 8 --data_dir ~/Data/abaw5 --features res18_pip --loss l2 \
---test_ckpt experiments/eri/version_207/checkpoints/best-epoch=11-val_apcc=0.363.ckpt \
---data_dir dataset/ --features features/ --aligned aligned/ \
+--test_ckpt /home/yini/ABAW5/experiments/eri/res18_au_377/checkpoints/best-epoch=10-val_apcc=0.377.ckpt  \
+--data_dir /data/abaw5/ --features res18_openface_features/ --aligned openface_align/ \
 --train False
 
 
